4c6008da85a16ed4467b74ad433936e803286ad9
socratone <gim2origin@gmail.com>
2021-04-11 21:00:58 +0900

    thunk 함수 주석 추가

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,12 @@
 import { GET_TEXT, GET_TEXT_SUCCESS, GET_TEXT_ERROR } from './constants';
 
+/**
+ * 일반적으로 actions에는 action이 정의된 객체를 리턴하는 함수가 와야 하지만
+ * thunk를 사용하는 경우 아래처럼 dispatch와 getState를 파라미터로 하는
+ * 함수를 리턴할 수 있다.
+ * 이 함수 안에서 async나 Promise를 사용하여 비동기 처리가 가능하다.
+ */
+
 export const getText = () => async (dispatch, getState) => {
   dispatch({ type: GET_TEXT })
 
